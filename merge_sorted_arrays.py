c3822406b530da4a419b9cba690cda1948425a82
akshayshembekar <akshay.p.shembekar@live.com>
2022-05-26 03:54:14 -0400

    5/26:APS - Merge 2 arrays into 1

---
+++ merge_sorted_arrays.py
@@ -0,0 +1,36 @@
+
+def merge_one_into_another(first, second):
+    """
+    Args:
+     first(list_int32)
+     second(list_int32)
+    Returns:
+     list_int32
+    """
+    # Write your code here.
+    i = len(first)-1
+    j = len(first)-1
+    k = len(second)-1
+
+    while i >= 0 and j >= 0:
+        if first[i] <= second[j]:
+            second[k] = second[j]
+            j -= 1
+            k -= 1
+        else:
+            second[k] = first[i]
+            i -= 1
+            k -= 1
+
+    while i >= 0:
+        second[k] = first[i]
+        i -= 1
+        k -= 1
+
+    return second
+
+
+first = [4, 5, 6]
+second = [1, 2, 3, 0, 0, 0]
+result = merge_one_into_another(first, second)
+print(result)
